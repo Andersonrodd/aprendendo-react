d8b78649477def794c0dea460459fedd3684ee5f
Anderson Rodrigo <andersonrodrigoam@gmail.com>
2022-07-08 19:30:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Support from "./Components/Support";
+import AllInOne from "./Components/AllInOne";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+    </>
+  );
+}
+
+export default App;
